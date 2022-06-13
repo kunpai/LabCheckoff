90cbf9b874754c6ba1df198b221a6c07ed72bdfa
kunpai <kunpai@ucdavis.edu>
2022-06-12 17:11:54 -0700

    first commit

---
+++ game.py
@@ -0,0 +1,135 @@
+import pygame
+import random
+pygame.init()
+win = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Board Collection")
+my_image = pygame.image.load('ghiasi.png').convert()
+smaller = pygame.transform.scale(my_image, (100, 125))
+board = pygame.image.load('CC3200.png').convert()
+smallr = pygame.transform.scale(board, (50, 50))
+gary = pygame.image.load('gary.png').convert()
+small = pygame.transform.scale(gary, (100, 100))
+# defining a font
+smallfont = pygame.font.SysFont('Corbel',35)
+text = smallfont.render('quit' , True , (255,0,0))
+x = 100
+y = 100
+baddyX = 300
+baddyY = 300
+vel = 6
+baddyVel = 4
+run = True
+ctr = 0
+# stores the width of the
+# screen into a variable
+width = win.get_width()
+
+# stores the height of the
+# screen into a variable
+height = win.get_height()
+
+def score():
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("Boards Collected: "+str(ctr), True, (255,255,255))
+    win.blit(text,(0,0))
+
+def draw_game():
+    win.fill((0, 0, 0))
+    win.blit(smaller, (x, y))
+    win.blit(smallr, boardCoor)
+    score()
+    win.blit(small, (baddyX, baddyY))
+    win.blit(small, (1280-baddyX, 720-baddyY))
+    #pygame.draw.rect(win, (255, 0, 0), (baddyX, baddyY, 40, 40))
+    pygame.display.update()
+
+
+def randomBox():
+    return (random.randint(0, 1000), random.randint(0, 600))
+
+
+def generateBoard():
+    win.blit(smallr, randomBox())
+    pygame.display.update()
+
+
+def checkCollision(x, y, boardX, boardY):
+    ghiasiSize = (100, 125)
+    boardSize = (50, 50)
+    if x + ghiasiSize[0] > boardX and x < boardX + boardSize[0] and y + ghiasiSize[1] > boardY and y < boardY + boardSize[1]:
+        return True
+    return False
+
+
+boardCoor = randomBox()
+
+while run:
+    mouse = pygame.mouse.get_pos()
+    pygame.time.delay(50)
+    if baddyX < x - 110:
+        baddyX = baddyX + baddyVel
+
+    elif baddyX > x + 110:
+        baddyX = baddyX - baddyVel
+
+    elif baddyY < y - 110:
+        baddyY = baddyY + baddyVel
+
+    elif baddyY > y + 110:
+        baddyY = baddyY - baddyVel
+    else:
+        run = False
+
+    if checkCollision(x, y, boardCoor[0], boardCoor[1]):
+        boardCoor = randomBox()
+        ctr = ctr + 1
+        print(ctr)
+        # draw_game()
+      #   if ctr == 10:
+      #       print("You win!")
+      #       run = False
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:	
+			#if the mouse is clicked on the
+			# button the game is terminated
+            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
+                run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        x -= vel
+        if (x<=0):
+              x = 0
+        if (x>=1155):
+              x = 1155
+
+    if keys[pygame.K_RIGHT]:
+        x += vel
+        if (x<=0):
+              x = 0
+        if (x>=1155):
+              x = 1155
+
+    if keys[pygame.K_UP]:
+        y -= vel
+        if (y<=0):
+              y = 0
+        if (y>=620):
+              y = 620
+
+    if keys[pygame.K_DOWN]:
+        y += vel
+        if (y<=0):
+              y = 0
+        if (y>=620):
+              y = 620
+
+    draw_game()
+
+
+pygame.quit()
