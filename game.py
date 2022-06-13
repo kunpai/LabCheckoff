94973e3e3b4ffcb8c67c7e006efbeb3930e2b31d
Parth Shah <helloparthshah@gmail.com>
2022-06-12 21:14:24 -0700

    changes some sprites

--- game.py
+++ game.py
@@ -5,16 +5,19 @@ pygame.init()
 
 win = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Board Collection")
-my_image = pygame.image.load('ghiasi.png').convert()
-smaller = pygame.transform.scale(my_image, (100, 125))
-board = pygame.image.load('CC3200.png').convert()
-smallr = pygame.transform.scale(board, (50, 50))
-gary = pygame.image.load('gary.png').convert()
-small = pygame.transform.scale(gary, (100, 100))
-heart = pygame.image.load('heart.png').convert()
+my_image = pygame.image.load('ghiasi.png').convert_alpha()
+smaller = pygame.transform.scale(
+    my_image, (my_image.get_width()/4, my_image.get_height()/4))
+board = pygame.image.load('CC3200.png').convert_alpha()
+# scale the width to 50%
+board = pygame.transform.scale(
+    board, (board.get_width()/4, board.get_height()/4))
+gary = pygame.image.load('gary.png').convert_alpha()
+small = pygame.transform.scale(gary, (gary.get_width()/4, gary.get_height()/4))
+heart = pygame.image.load('heart.png').convert_alpha()
 heart = pygame.transform.scale(heart, (40, 40))
 
-title = pygame.image.load('title.png').convert()
+title = pygame.image.load('title.png').convert_alpha()
 # defining a font
 smallfont = pygame.font.SysFont('Corbel', 35)
 text = smallfont.render('quit', True, (255, 0, 0))
@@ -54,7 +57,7 @@ def score():
 def draw_game():
     win.fill((0, 0, 0))
     win.blit(smaller, (x, y))
-    win.blit(smallr, boardCoor)
+    win.blit(board, boardCoor)
     score()
     # draw a floating health bar
     #win.blit(small, (1280-baddyX, 720-baddyY))
@@ -66,7 +69,7 @@ def randomBox():
 
 
 def generateBoard():
-    win.blit(smallr, randomBox())
+    win.blit(board, randomBox())
 
 
 def checkCollision(x, y, size1, boardX, boardY, size2):
@@ -114,9 +117,10 @@ class Gary:
     def draw(self):
         win.blit(small, (self.x, self.y))
         # display health bar under gary
-        pygame.draw.rect(win, (255, 0, 0), (self.x, self.y+100, 100, 10))
+        pygame.draw.rect(win, (255, 0, 0),
+                         (self.x, self.y+small.get_height(), small.get_width(), 10))
         pygame.draw.rect(win, (0, 255, 0), (self.x, self.y +
-                         100, 100*(self.health/100), 10))
+                         small.get_height(), small.get_width()*(self.health/100), 10))
 
 
 class Heart:
@@ -158,24 +162,19 @@ class Bullet:
         # check if bullet is in
         # check for collision with baddy or player
         for gary in garys:
-            if checkCollision(self.x, self.y, (10, 10), gary.x, gary.y, (100, 100)):
+            if checkCollision(self.x, self.y, (10, 10), gary.x, gary.y, (small.get_width(), small.get_height())):
                 # delete bullet
                 if(self.owner == "sohail"):
                     sohailBullets.remove(self)
                     gary.health -= 10
                     break
                     print("collision")
-        if checkCollision(self.x, self.y, (10, 10), x, y, (100, 125)):
+        if checkCollision(self.x, self.y, (10, 10), x, y, (smaller.get_width(), smaller.get_height())):
             # delete bullet
             if(self.owner == "baddy"):
                 global health
                 health -= 5
                 global hearts
-                # 5% chance of generating a heart
-                if(random.randint(0, 100) <= 5):
-                    if(len(hearts) < 5):
-                        hearts.append(Heart(random.randint(0, width),
-                                            random.randint(0, height)))
                 bullets.remove(self)
                 print("collision with sohail")
                 if(health <= 0):
@@ -221,7 +220,7 @@ def mainGame():
     global ctr, boardCoor, baddyX, baddyY, vel, baddyVel, x, y, width, height, win
     global counter
     counter += 1
-    if(counter == 500):
+    if(counter == 200):
         counter = 0
         if(len(garys) < 5):
             garys.append(Gary(random.randint(0, width),
@@ -229,7 +228,7 @@ def mainGame():
     clock.tick(60)
     mouse = pygame.mouse.get_pos()
     pygame.time.delay(50)
-    if checkCollision(x, y, (100, 125), boardCoor[0], boardCoor[1], (50, 50)):
+    if checkCollision(x, y, smaller.get_size(), boardCoor[0], boardCoor[1], board.get_size()):
         boardCoor = randomBox()
         ctr = ctr + 1
         print(ctr)
@@ -238,6 +237,11 @@ def mainGame():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
+            # 5% chance of generating a heart
+            if(random.randint(0, 100) <= 5):
+                if(len(hearts) < 5):
+                    hearts.append(Heart(random.randint(0, width),
+                                        random.randint(0, height)))
             norm = math.sqrt((mouse[0]-x)**2 + (mouse[1]-y)**2)
             vector = (-(x-mouse[0])/norm*20, -(y-mouse[1])/norm*20)
             bullet = Bullet(x+100/2, y+125/2, vector, "sohail")
